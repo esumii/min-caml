d4bf27dfc3eb589c36fb5515e3d3eacfba2489c2
Eijiro Sumii <eijiro.sumii@gmail.com>
2019-08-14 18:47:05 +0900

    String -> Bytes

--- min-rt/ppm.ml
+++ min-rt/ppm.ml
@@ -1,31 +1,31 @@
 open Bigarray
 
-type pixmap = string * int
+type pixmap = bytes * int
 
 let get (img, width) i j k =
-  Char.code (img.[((j * width) + i) * 3 + k])
+  Char.code (Bytes.get img (((j * width) + i) * 3 + k))
 
 let set (img, width) i j k v =
-  img.[((j * width) + i) * 3 + k] <- Char.unsafe_chr v
+  Bytes.set img (((j * width) + i) * 3 + k) (Char.unsafe_chr v)
 
 let setp (img, width) i j r g b =
   let p = ((j * width) + i) * 3 in
-  img.[p]     <- Char.unsafe_chr r;
-  img.[p + 1] <- Char.unsafe_chr g;
-  img.[p + 2] <- Char.unsafe_chr b
+  Bytes.set img p (Char.unsafe_chr r);
+  Bytes.set img (p + 1) (Char.unsafe_chr g);
+  Bytes.set img (p + 2) (Char.unsafe_chr b)
 
-let init ~width ~height = (String.create (height * width * 3), width)
+let init ~width ~height = (Bytes.create (height * width * 3), width)
 
 let width (s, width) = width
-let height (s, width) = String.length s / width / 3
+let height (s, width) = Bytes.length s / width / 3
 
 let dump file (img, width) =
-  let sz = String.length img in
+  let sz = Bytes.length img in
   let height = sz / 3 / width in
   let f = open_out_bin file in
   output_string f "P6\n# PL Club\n";
   Printf.fprintf f "%d %d\n255\n" width height;
-  output_string f img;
+  output_bytes f img;
   close_out f
 
 let load file =
@@ -39,6 +39,6 @@ let load file =
     int_of_string (String.sub s (!i + 1) (String.length s - !i - 1)) in
   assert (input_line f = "255");
   let (s, _) as img = init width height in
-  really_input f s 0 (String.length s);
+  really_input f s 0 (Bytes.length s);
   close_in f;
   img
